05b163f18f074bdead856d080daf8c1b75a5b61b
Fernando Suares <fernandoaqs@outlook.com.br>
2024-02-02 14:34:14 -0300

    🐛 FIx shell theme applying bug

--- themes.sh
+++ themes.sh
@@ -23,11 +23,12 @@ unzip Dracula.zip -d ~/Downloads/Icons
 sudo cp -r ~/Downloads/Themes/Dracula ~/.themes /usr/share/themes
 sudo cp -r ~/Downloads/Icons/Dracula ~/.icons /usr/share/icons
 
+sudo cp $HOME/.local/share/gnome-shell/extensions/user-theme@gnome-shell-extensions.gcampax.github.com/schemas/org.gnome.shell.extensions.user-theme.gschema.xml /usr/share/glib-2.0/schemas
+sudo glib-compile-schemas /usr/share/glib-2.0/schemas
+
 gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
 gsettings set org.gnome.desktop.wm.preferences theme "Dracula"
-
 gsettings set org.gnome.shell.extensions.user-theme name "Dracula"
-
 gsettings set org.gnome.desktop.interface icon-theme "Dracula"
 
 
